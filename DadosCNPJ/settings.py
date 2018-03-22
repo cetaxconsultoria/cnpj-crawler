bc94b19779cea0ffe464a8d42d4578669be879bf
Hugo Padovani <hgpadovani@gmail.com>
2018-03-22 16:00:46 -0300

    a

--- DadosCNPJ/settings.py
+++ DadosCNPJ/settings.py
@@ -122,3 +122,14 @@ STATIC_URL = '/static/'
 
 #django_heroku.settings(locals())
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(_file_))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
