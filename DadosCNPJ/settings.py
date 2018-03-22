f89b4980d5ca67ed089657bdef73c1c382c29218
Hugo Padovani <hgpadovani@gmail.com>
2018-03-22 13:31:12 -0300

    adding

--- DadosCNPJ/settings.py
+++ DadosCNPJ/settings.py
@@ -119,6 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn dadoscnpj.wsgi
+web: gunicorn DadosCNPJ.wsgi
